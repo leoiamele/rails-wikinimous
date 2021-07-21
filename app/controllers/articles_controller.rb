6fee229cfa17433fcc4d1a0e6d65af3b6914c742
Leandro Iamele <leandroiamele@gmail.com>
2021-07-21 11:52:10 -0300

    add article model, articles controller, index action and index view

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Article.delete_all
+
+10.times do
+  new_article = Article.new(title: Faker::Books::CultureSeries.book, 
+    content: Faker::Lorem.sentence(word_count: (35..50).to_a.sample, supplemental: true, random_words_to_add: 4))
+  new_article.save
+  puts new_article.title
+  puts new_article.content
+end
