3e3e19b4ab4d06bce83b79c98ce41fead132022e
Leandro Iamele <leandroiamele@gmail.com>
2021-07-22 07:29:41 -0300

    add create, update and destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,49 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article), notice: 'Article was successfully created.'
+    else
+      render :new
+    end
+    # @article = Article.create(article_params)
+    # redirect_to article_path(@article)
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else
+      render :edit
+    end
+    # @article = Article.find(params[:id])
+    # @article.update(article_params)
+    # redirect_to article_path(@article) 
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.delete
+    redirect_to articles_path
+  end
+
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
